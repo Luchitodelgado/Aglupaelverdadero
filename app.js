1f02db9a4371aa8fc27d9f5071beb1df6d530125
Agustina <agusti_zarate@hotmail.com>
2022-05-05 16:52:59 -0300

    Todo listo

---
+++ app.js
@@ -0,0 +1,28 @@
+// Requerimos express y lo ejecutamos para tener disponibles todos los metodos que vamos a precisar
+const express = require("express");
+const app = express();
+
+// Modulo nativo para manejar las rutas de los archivos
+const path = require("path");
+
+// Usando recursos estáticos.
+app.use(express.static("public"));
+
+// Ponemos a escuchar el servidor
+app.listen(3030, () => {
+    console.log("Servidor corriendo en http://localhost:AGLUPA")
+});
+
+// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+app.get ("/", (req,res) => {
+    let htmlPath = path.resolve(__dirname, "./views/home.html");
+    res.sendFile(htmlPath)
+});
+app.get ("/ingresa", (req,res) => {
+    let htmlPath = path.resolve(__dirname, "./views/ingresa.html");
+    res.sendFile(htmlPath)
+});
+app.get ("/registrarte", (req,res) => {
+    let htmlPath = path.resolve(__dirname, "./views/registrarte.html");
+    res.sendFile(htmlPath)
+});
