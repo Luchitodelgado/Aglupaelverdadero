93e6d9db8745777f8e64c6fdac241d7dcc16dac9
Patri0506 <patrudilernia_bea@live.com>
2022-06-03 17:58:42 -0300

    c

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,5 +4,4 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 router.get("/registrarte", userController.registro)
 router.get("/ingresa", userController.ingresar)
-router.get("/carrito", userController.carrito)
 module.exports = router;
