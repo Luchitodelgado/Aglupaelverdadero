19c0b3d1ec6febbd59e8d3040a19f7ab607f37ac
ludmi2501 <ludmi1628@gmail.com>
2022-06-13 19:47:58 -0300

    ruta borcegos

--- src/controllers/borcegosController.js
+++ src/controllers/borcegosController.js
@@ -5,13 +5,21 @@ const borcegosFilePath = path.join(__dirname, '../data/borcegos.json');
 const borcegos = JSON.parse(fs.readFileSync(borcegosFilePath, 'utf-8'));
 
 const controller = {
-	// Root - Show all products
 	borcegos: (req, res) => {
         let id = req.params.id
 		res.render('borcegos', {
 			borcegos
 		})
 	},
+
+    detail: (req, res) => {
+		let id = req.params.id;
+ 
+		res.render('detail', {
+			product
+		})
+	},
+
 };
 
 module.exports = controller;
