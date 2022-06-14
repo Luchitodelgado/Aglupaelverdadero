9f05d8fb7c5648f65e84d7d77c904bf7c2de2b49
ludmi2501 <ludmi1628@gmail.com>
2022-06-14 10:55:59 -0300

    formulario

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const path = require("path");
+const methodOverride = require('method-override');
 
 const mainRouter = require('./src/routes/mainRouter'); // Rutas main
 const userRouter = require('./src/routes/userRouter'); // Rutas main
@@ -13,7 +14,10 @@ const botas = require ("./src/routes/botasRouter")
 app.set('view engine', 'ejs'); 
 app.set('views', path.join(__dirname, '/src/views'));
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '../public'))); 
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.json()); 
+app.use(methodOverride('_method'));
 
 app.use('/', mainRouter);
 app.use('/', userRouter);
