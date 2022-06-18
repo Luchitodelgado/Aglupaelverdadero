8dc978c9d2889ca8fe78d4c617037d188dd63d08
ludmi2501 <ludmi1628@gmail.com>
2022-06-17 23:00:40 -0300

    poniendo multer en registrarte

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const multer = require ('multer');
+const path = require('path');
 
 const userController = require('../controllers/usersController');
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "../../public/img/img-usuarios"));
+    },
+    filename: (req,file, cb) => {
+        console.log(file);
+        const newFilename = "usuario" + Date.now() + path.extname(file.originalname);
+        cb (null, newFilename);
+    }
+});
+
+const upload = multer ({storage})
+
 router.get("/registrarte", userController.registro)
 router.get("/ingresa", userController.ingresar)
-router.post('/users', userController.store);
+router.post('/users', upload.single("usuario"), userController.store);
 router.get('/users', userController.create )
 module.exports = router;
