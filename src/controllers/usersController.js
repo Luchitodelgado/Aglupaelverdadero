8dedcdc3228dfbcf52f3492ddae240d9aedbded2
Luchitodelgado <contactldelgado@gmail.com>
2022-07-19 14:37:22 -0300

    actualizacion de contrasena

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 const usersFilePath = path.join(__dirname, '../data/usuarios.json');
-const {validationResult}=require('express-validator')
-const bcryptjs=require('bcryptjs')
+const { validationResult } = require('express-validator')
+const bcrypt = require('bcryptjs')
 
 
 const controller = {
@@ -13,33 +13,41 @@ const controller = {
 	ingresa: (req, res) => {
 		res.render("ingresa")
 	},
-	processLogin: function(req,res){
+	processLogin: function (req, res) {
 		let users = usersFilePath;
 		let errors = validationResult(req);
 		let usuarioAloguearse;
-		if(errors.isEmpty()){
-		
-		for (let i = 0; i < users.length; i++){
-			if (users[i].email == req.body.email){
-				if(bcrypt.compareSync(req.body.password, users[i].password)){
-				 usuarioAloguearse = users[i];
+
+
+		if (errors.isEmpty()) {
+
+			for (let i = 0; i < users.length; i++) {
+				if (users[i].email == req.body.email) {
+					if (bcrypt.compareSync(req.body.password, users[i].password2)) {
+						usuarioAloguearse = users[i];
+						console.log('accediste')
+						console.log(usuarioAloguearse);
+					}
 				}
 			}
+			if (usuarioAloguearse == undefined) {
+				console.log('incorrecto')
+				return res.render('home', {
+					errors: [
+						{ msg: "Credenciales invalidas" }
+					]
+			
+				});
+			}
+			req.session.usuarioLogueado = usuarioAloguearse;
+			
+		} else {
+			return res.render("home", { errors: errors.errors });
 		}
-		if(usuarioAloguearse == undefined){
-			return res.render('home',{errors: [
-				{msg:"Credenciales invalidas"}
-			]});
-		}
-		req.session.usuarioLogueado = usuarioAloguearse;
-		res.redirect("/");
-		}else{
-			return res.render("/",{errors: errors.errors});
-		}
-		console.log(usuarioAloguearse);
 	},
 	// Create -  Method to store
 	store: (req, res) => {
+		let pass = bcrypt.hashSync(req.body.password2, 10);
 
 		const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 		let newUser = {
@@ -47,8 +55,9 @@ const controller = {
 			nombreuser: req.body.nombreuser,
 			email: req.body.email,
 			fechadenacimiento: req.body.fechadenacimiento,
-			avatar:req.file.filename,
-			password: bcryptjs.hashSync(req.body.password, 10)
+			avatar: req.file.filename,
+			password2: pass
+
 		}
 		users.push(newUser);
 		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, ' '));
@@ -56,7 +65,9 @@ const controller = {
 	},
 	create: (req, res) => {
 		res.render('ingresa')
-	}
+	},
+
 };
+
 
 module.exports = controller;
