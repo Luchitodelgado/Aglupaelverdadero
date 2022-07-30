b0060d9e432296f1d180b6a8b462fcb2b77d6554
Luchitodelgado <contactldelgado@gmail.com>
2022-07-30 15:06:32 -0300

    algunos comentarios al app

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
+/* ------------------------------ EL ORDEN DE TODOS ESTOS BLOQUES ES IMPORTANTE------------------------------*/
+/* ------------------------------ REQUERIMIENTOS ------------------------------*/
 const express = require("express");
 const app = express();
 const path = require("path");
 const methodOverride = require('method-override');
-
-
-const mainRouter = require('./src/routes/mainRouter'); // Rutas main
-const userRouter = require('./src/routes/userRouter'); // Rutas main
-const productos = require ("./src/routes/productsRouter");
+const mainRouter = require('./src/routes/mainRouter');
+const userRouter = require('./src/routes/userRouter');
+const productos = require("./src/routes/productsRouter");
 const session = require('express-session');
 const { create } = require("domain");
-
-app.set('view engine', 'ejs'); 
+/* ------------------------------ MOTOR DE PLANTILLAS (TEMPLATE ENGINES) ------------------------------*/
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/src/views'));
-
+/* ------------------------------ MEDIOAMBIENTE DE LA APLICACION (MIDDLEWARES)------------------------------*/
 app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, '../public')));  // Necesario para los archivos estáticos en el folder /public
-app.use(express.urlencoded({ extended: false })); // Para que se usa??
-app.use(express.json()); // Para que se usa??
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(methodOverride('_method'));
-
-app.use(session({secret:"Mensaje secreto"}));
+app.use(session({ secret: "Mensaje secreto" }));
 app.use('/', mainRouter);
 app.use('/', userRouter);
 app.use('/', productos);
 
-
+/* ------------------------------ PONER EN MARCHA EL SV------------------------------*/
 app.listen(3000, () => {
-    console.log("Servidor corriendo en http://localhost:3000")});
+    console.log("Servidor corriendo en http://localhost:3000")
+});
