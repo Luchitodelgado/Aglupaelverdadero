99349836c1965e82bbbd12b182f683a51a9ccca1
Agus <zagustina708@gmail.com>
2022-08-10 20:00:07 -0300

    seguimos validando login

--- public/js/loginForm.js
+++ public/js/loginForm.js
@@ -2,28 +2,35 @@ window.addEventListener("load",function(){
     let formularioLogin = document.querySelector("loginForm");
 
     formularioLogin.addEventListener("submit",function(e){
-        e.preventDefault();
+        let erorrs =[];
 
         let campoEmail= document.getElementById("loginEmail");
         let campoPassword= document.getElementById("password");
 
         if(campoEmail.value.length == ""){
-            console.log("Este campo es obligatorio")
-        };
+            erorrs.push("Este campo es obligatorio")
+        }
         if(campoEmail.value == validator){
-            console.log("Este campo debe contener un email válido")
-        };
+            erorrs.push("Este campo debe contener un email válido")
+        }
 
         if(campoPassword.value == ""){
-            console.log("Este campo es obligatorio")
+            erorrs.push("Este campo es obligatorio")
         }
         if(campoPassword.value.length >= 8){
-            console.log("La contraseña debe tener al menos 8 caracteres")
+            erorrs.push("La contraseña debe tener al menos 8 caracteres")
         }
-        
+
+        if(erorrs.length > 0){
+            e.preventDefault();
+        };
+
+        let ulErrors = document.querySelector(".errors")
+        for (let i = 0; i < erorrs.length; i++) {
+            ulErrors.innnerHTML += "<li>" + erorrs[i] + "</li>"
+            
+        }
+
     })
-
-
-
 
 })
