22c949a587c4ad6041a8f448540d46fa938308fe
Agus <zagustina708@gmail.com>
2022-08-17 16:43:01 -0300

    guardando login nuevo

--- public/js/loginForm.js
+++ public/js/loginForm.js
@@ -1,36 +1,91 @@
-window.addEventListener("load",function(){
-    let formularioLogin = document.querySelector("loginForm");
+const formulario = document.getElementById('formularioLogin');
+const inputs = document.querySelectorAll('#formularioLogin input');
 
-    formularioLogin.addEventListener("submit",function(e){
-        let erorrs =[];
+const expresiones = {
+  password: /^.{8,12}$/, // 4 a 12 digitos.
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+}
 
-        let campoEmail= document.querySelector("form.loginEmail");
-        let campoPassword= document.querySelector("#loginPassword");
+const campos = {
+  password: false,
+  email: false
+}
 
-        if(campoEmail.value == ""){
-            erorrs.push("Este campo es obligatorio")
-        }
-        if(campoEmail.value == validator){
-            erorrs.push("Este campo debe contener un email válido")
-        }
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "password":
+      validarCampo(expresiones.password, e.target, 'password');
+      validarPassword2();
+      break;
+    case "password2":
+      validarPassword2();
+      break;
+    case "email":
+      validarCampo(expresiones.email, e.target, 'email');
+      break;
+  }
+}
+const validarCampo = (expresion, input, campo) => {
+  if (expresion.test(input.value)) {
+    document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+    document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+    document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
+    document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
+    document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+    campos[campo] = true;
+  } else {
+    document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+    document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+    document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
+    document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
+    document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+    campos[campo] = false;
+  }
+}
 
-        if(campoPassword.value == ""){
-            erorrs.push("Este campo es obligatorio")
-        }
-        if(campoPassword.value.length >= 8){
-            erorrs.push("La contraseña debe tener al menos 8 caracteres")
-        }
+const validarPassword2 = () => {
+  const inputPassword1 = document.getElementById('password');
+  const inputPassword2 = document.getElementById('password2');
 
-        if(erorrs.length > 0){
-           e.preventDefault();
-        };
+  if (inputPassword1.value !== inputPassword2.value) {
+    document.getElementById(`grupo__password2`).classList.add('formulario__grupo-incorrecto');
+    document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-correcto');
+    document.querySelector(`#grupo__password2 i`).classList.add('fa-times-circle');
+    document.querySelector(`#grupo__password2 i`).classList.remove('fa-check-circle');
+    document.querySelector(`#grupo__password2 .formulario__input-error`).classList.add('formulario__input-error-activo');
+    campos['password'] = false;
+  } else {
+    document.getElementById(`grupo__password2`).classList.remove('formulario__grupo-incorrecto');
+    document.getElementById(`grupo__password2`).classList.add('formulario__grupo-correcto');
+    document.querySelector(`#grupo__password2 i`).classList.remove('fa-times-circle');
+    document.querySelector(`#grupo__password2 i`).classList.add('fa-check-circle');
+    document.querySelector(`#grupo__password2 .formulario__input-error`).classList.remove('formulario__input-error-activo');
+    campos['password'] = true;
+  }
+}
 
-        let ulErrors = document.querySelector(".errors")
-        for (let i = 0; i < erorrs.length; i++) {
-            ulErrors.innnerHTML += "<li>" + erorrs[i] + "</li>"
-            
-        }
+inputs.forEach((input) => {
+  input.addEventListener('keyup', validarFormulario);
+  input.addEventListener('blur', validarFormulario);
+});
 
-    })
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-})
+  const terminos = document.getElementById('terminos');
+  if (campos.password && campos.email && terminos.checked) {
+    formulario.submit();
+
+
+    document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+    setTimeout(() => {
+      document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+    }, 5000);
+
+    document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+      icono.classList.remove('formulario__grupo-correcto');
+    });
+  } else {
+    document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+  }
+});
--- retro.md
+++ retro.md
@@ -3,14 +3,11 @@
 <h2><strong>COMENZAR A HACER</strong><h2>
 <p>Afianzarnos como grupo y aprender a trabajar en equipo de a dos. Conocer fortalezas y debilidades de cada uno y explotar al máximo lo que podemos aportar individualmente al grupo.<p>
 
-<h2><strong>HACER MÁS</strong> <h2>
+<h2><strong>HACER MÁS</strong><h2>
 <p>Explotar destrezas individuales, apoyarnos en el otro en las actividades que nos cuestan más o en las que necesitamos ayuda y no nos sentimos tan cómodos o tenemos dudas.<p>
 
 <h2>CONTINUAR HACIENDO<h2>
-<p>Seguir manteniendo comunicación constante de que hicimos ayer, si alguna tarea quedó pendiente plantear si hay que comenzar una busqueda de información para lograrlo, etc; y qué tareas vamos a realidar con prioridad el día de hoy.</p>
-
-<h2>HACER MENOS<h2>
-<p></p>
+<p>Seguir manteniendo comunicación constante de que hicimos ayer, si alguna tarea quedó pendiente plantear si hay que comenzar una busqueda de información para lograrlo, etc.Qué tareas vamos a realizar con prioridad el día de hoy.</p>
 
 <h2>DEJAR DE HACER</h2>
 <p>No enfocarnos en qué hacen o no los participantes del grupo, enfocarnos en los avances y en como nos propusimos encarar este sprint.</p>
